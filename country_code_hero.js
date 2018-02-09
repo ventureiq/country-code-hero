e7acc7d90b632e789e6d7e81305a4a2731663ad5
Timir <tkaria@gmail.com>
2018-02-09 11:21:45 +0100

    Initial commit

---
+++ country_code_hero.js
@@ -0,0 +1,114 @@
+/**
+ * Utilities for dealing with ISO 3166-1 style country codes
+ * Underlying data from https://github.com/lukes/ISO-3166-Countries-with-Regional-Codes
+ * Example usage:
+ *
+ */
+class IsoCountryCodes {
+  /**
+   * Create an country codes object
+   * @param {Object} countryFile
+   */
+  constructor(countryFile = require('./iso_country_codes.json')) {
+    this._map = countryFile;
+  }
+
+  /**
+   * Search in full name, alpha-2 and alpha-3 for a country code with a fragment
+   * @param {String} partial
+   * @return {Array} of potential matches
+   */
+  search(partial) {
+    let re = new RegExp(partial, 'ig');
+    return this._map.filter(function(value) {
+      return re.exec(value.name) ||
+        re.exec(value['alpha-2']) ||
+        re.exec(value['alpha-3']);
+    });
+  }
+
+  /**
+   * Find an exact match for name in name, alpha-2, and alpha-3 fields
+   * @param {String} name
+   * @return {Array} of matches
+   */
+  find(name) {
+    name = name.toLocaleUpperCase();
+    return this._map.filter(function(value) {
+      return value.name === name ||
+        value['alpha-2'] === name ||
+        value['alpha-3'] === name;
+    });
+  }
+
+  /**
+   * Determine if match exists in alpha-3 field only
+   * NOTE: Does not return the value
+   * @param {String} alpha3
+   * @return {Boolean} True if exists; else False
+   */
+  findAlpha3(alpha3) {
+    alpha3 = alpha3.toLocaleUpperCase();
+    return this._map.find(function(value) {
+      return value['alpha-3'] === alpha3;
+    });
+  }
+
+  /**
+   * Determine if match exists in alpha-3 field only
+   * NOTE: Does not return the value
+   * @param {String} alpha2
+   * @return {Boolean} True if exists; else False
+   */
+  findAlpha2(alpha2) {
+    alpha2 = alpha2.toLocaleUpperCase();
+    return this._map.find(function(value) {
+      return value['alpha-2'] === alpha2;
+    });
+  }
+
+  /**
+   * Get array of all alpha-3 codes
+   * @return {Array} of all alpha-3 codes
+   */
+  getAlpha3() {
+    return this._map.map(function(value) {
+      return value['alpha-3'];
+    });
+  }
+
+  /**
+   * Get array of all alpha-2 codes
+   * @return {Array} of all alpha-2 codes
+   */
+  getAlpha2() {
+    return this._map.map(function(value) {
+      return value['alpha-2'];
+    });
+  }
+
+  /**
+   * Get array of all region names
+   * E.g. Asia, Americas, etc.
+   * @param {String} region
+   * @return {Array} of all region names
+   */
+  getRegion(region) {
+    return this._map.filter(function(value) {
+      return value['region'] === region;
+    });
+  }
+
+  /**
+   * Return array of all alpha-2 country codes in EEA region
+   * @return {Array} all alpha-2 country codes in EEA region
+   */
+  getEEA() {
+    return ['AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EU', 'EE', 'FI',
+            'FR', 'DE', 'GR', 'HU', 'IS', 'IE', 'IT', 'LV', 'LI', 'LT',
+            'LU', 'MT', 'NL', 'NO', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES',
+            'SE', 'CH', 'GB', 'UK'];
+  }
+}
+
+module.exports = IsoCountryCodes;
---
+++ test/country_code_hero.spec.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const assert = chai.assert;
+
+let IsoCountryCodes = require('../country_code_hero.js');
+const CountryCodes = require('../iso_country_codes.json');
+
+describe('IsoCountryCodes', () => {
+  it('needs require resolved with first IsoCountryCodes new object', function (done) {
+    let isoCC = new IsoCountryCodes();
+    setTimeout(() => {
+      assert.deepEqual(isoCC._map, CountryCodes);
+      done();
+    }, 0);
+  });
+
+  it('has 30counter in EEA', () => {
+    let isoCC = new IsoCountryCodes();
+    assert.isArray(isoCC.getEEA());
+    assert.strictEqual(isoCC.getEEA().length, 34);
+  });
+
+  it('recognizes regions', () => {
+    let isoCC = new IsoCountryCodes();
+    assert.isArray(isoCC.getRegion('Europe'));
+    assert.strictEqual(isoCC.getRegion('Europe').length, 51);
+  });
+
+  it('can display map of 2 letter country codes', () => {
+    let isoCC = new IsoCountryCodes();
+    let alpha2 = isoCC.getAlpha2();
+    assert.isArray(alpha2);
+    for (let code of alpha2) {
+      assert.isString(code);
+      assert.strictEqual(code.length, 2);
+    }
+  });
+
+  it('can display map of 3 letter country codes', () => {
+    let isoCC = new IsoCountryCodes();
+    let alpha3 = isoCC.getAlpha3();
+    assert.isArray(alpha3);
+    for (let code of alpha3) {
+      assert.isString(code);
+      assert.strictEqual(code.length, 3);
+    }
+  });
+
+  it('finds country by 2 letter country code', () => {
+    let isoCC = new IsoCountryCodes();
+    let nl = isoCC.findAlpha2('nl');
+    assert.isObject(nl);
+    assert.strictEqual(nl.name, 'Netherlands');
+  });
+
+  it('finds country by 3 letter country code', () => {
+    let isoCC = new IsoCountryCodes();
+    let nl = isoCC.findAlpha3('nld');
+    assert.isObject(nl);
+    assert.strictEqual(nl.name, 'Netherlands');
+  });
+
+  it('finds countries with exact match', () => {
+    let isoCC = new IsoCountryCodes();
+    let nl = isoCC.findAlpha3('nld');
+    let find = isoCC.find('nld');
+    assert.isArray(find);
+    assert.include(find, nl);
+  });
+
+  it('finds countries with regexp', () => {
+    let isoCC = new IsoCountryCodes();
+    let nl = isoCC.findAlpha3('nld');
+    let find = isoCC.search('nld');
+    assert.isArray(find);
+    assert.include(find, nl);
+  });
+});
+
